5e3566f0680a891cc51882428cde94fbcbb8b344
Konrad Pawlak <Konrad.Pawlak@student.hpi.uni-potsdam.de>
2023-07-23 19:55:05 +0200

    Coin Sound, Level 2

--- Assets/PacMan.cs
+++ Assets/PacMan.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SpeechIO;
 using static GameManager;
 
 public class PacMan : MonoBehaviour
 {
+    private SoundEffects soundEffects;
+
+    void Start()
+    {
+        soundEffects = GetComponent<SoundEffects>();
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
         {
+            soundEffects.coinCollected();
             Destroy(other.gameObject);
             //RefreshClosestCoinTEST();
         }
---
+++ Assets/SoundEffects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpeechIO;
+
+public class SoundEffects : MonoBehaviour
+{
+    public AudioClip coinSound;
+    private AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void coinCollected()
+    {
+        audioSource.PlayOneShot(coinSound);
+    }
+}
