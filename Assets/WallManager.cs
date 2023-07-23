c7beadc46b0f592ad2a772f8540557980b6b5ab5
Lars Schumann <larsschumann@mail.de>
2023-07-23 15:54:57 +0200

    FUCKING WALLS WORK

--- Assets/WallManager.cs
+++ Assets/WallManager.cs
@@ -1,33 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using DualPantoFramework;
+
 public class WallManager : MonoBehaviour
 {
     PantoCollider[] pantoColliders;
     void Start()
     {
+        activateColliders();
+    }
+
+    void activateColliders()
+    {
         pantoColliders = GameObject.FindObjectsOfType<PantoCollider>();
         foreach (PantoCollider collider in pantoColliders)
         {
+            
             collider.CreateObstacle();
             collider.Enable();
+            
         }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            foreach (PantoCollider collider in pantoColliders)
-            {
-                collider.Enable();
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            foreach (PantoCollider collider in pantoColliders)
-            {
-                collider.Disable();
-            }
-        }
+
     }
+
 }
