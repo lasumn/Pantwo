ee536a1d0306123769aefec7c61ffe58791ffe46
Lars Schumann <larsschumann@mail.de>
2023-07-24 01:53:41 +0200

    added sum wincon checkers

---
+++ Assets/WinConLevel1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinConLevel1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((CheckWinCondition()))
+        {
+            Debug.Log("Win condition met");
+            Invoke("LoadNextScene", 2f);
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        Debug.Log("Loading next scene");
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private bool CheckWinCondition()
+    {
+        if (GameObject.FindGameObjectsWithTag("Coin").Length > 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Assets/WinConLevel2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinConLevel2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float passedTime = 0f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        passedTime += Time.deltaTime;
+
+        if ((CheckWinCondition()))
+        {
+            Debug.Log("Win condition met");
+            Invoke("LoadNextScene", 2f);
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        Debug.Log("Loading next scene");
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private bool CheckWinCondition()
+    {
+        if (passedTime < 10f)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
